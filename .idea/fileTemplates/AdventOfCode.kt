0a7dcb8cf937c18c146cfda2f28bfc45c3070e90
Kirill Rakhman <kirill.rakhman.ext@adevinta.com>
2022-12-01 11:30:32 +0100

    commit .idea

---
+++ .idea/fileTemplates/AdventOfCode.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("${NAME}_test")
+    check(part1(testInput) == 0)
+
+    val input = readInput("${NAME}")
+    println(part1(input))
+    println(part2(input))
+}
